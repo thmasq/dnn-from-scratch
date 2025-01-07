63faa45199e5f0e025e42508c343d2b9f9568810
Arthur Carvalho <arthur_07q@hotmail.com>
2025-01-07 15:49:34 -0300

    Replace nalgebra with ndarray in the fully connected layer

--- src/fully_connected.rs
+++ src/fully_connected.rs
@@ -1,78 +1,67 @@
-use na::{DMatrix, RowDVector};
+use ndarray::{Array1, Array2, Axis};
 use rand::thread_rng;
 use rand_distr::{Distribution, Normal};
 
 /// Generate weights following the HE-Initialization method
-fn generate_weights(input_size: usize, output_size: usize) -> DMatrix<f64> {
+fn generate_weights(input_size: usize, output_size: usize) -> Array2<f64> {
     let scale = (2.0 / input_size as f64).sqrt();
     let normal = Normal::new(0.0, 1.0).unwrap();
     let mut rng = thread_rng();
-    let mut weights = DMatrix::zeros(input_size, output_size);
-    weights
-        .iter_mut()
-        .for_each(|element| *element = normal.sample(&mut rng) * scale);
-    weights
+    Array2::from_shape_fn((input_size, output_size), |_| {
+        normal.sample(&mut rng) * scale
+    })
 }
 
 pub struct FullyConnected<'a> {
     // Weights and biases
-    weights: DMatrix<f64>,
-    biases: RowDVector<f64>,
+    weights: Array2<f64>,
+    biases: Array1<f64>,
     // Activation-related
     activation: &'a str,
     // Optimizer-related (Adam)
-    m_weights: DMatrix<f64>,
-    v_weights: DMatrix<f64>,
-    m_biases: RowDVector<f64>,
-    v_biases: RowDVector<f64>,
+    m_weights: Array2<f64>,
+    v_weights: Array2<f64>,
+    m_biases: Array1<f64>,
+    v_biases: Array1<f64>,
     // Hyper-parameters (Adam)
     beta_1: f64,
     beta_2: f64,
     epsilon: f64,
     // Input and Output (for the forward pass)
-    input: DMatrix<f64>,
-    output: DMatrix<f64>,
+    input: Array2<f64>,
+    output: Array2<f64>,
 }
 
 impl FullyConnected<'_> {
     /// Initialize a fully-connected layer
-    ///
-    /// Args:
-    ///
-    /// input_size (int): Input shape of the layer
-    ///
-    /// output_size (int): Output of the layer
-    ///
-    /// activation (str): Activation function
     pub fn new<'a>(input_size: usize, output_size: usize, activation: &'a str) -> FullyConnected {
         FullyConnected {
             weights: generate_weights(input_size, output_size),
-            biases: RowDVector::zeros(input_size),
-            activation: &activation,
-            m_weights: DMatrix::zeros(input_size, output_size),
-            v_weights: DMatrix::zeros(input_size, output_size),
-            m_biases: RowDVector::zeros(input_size),
-            v_biases: RowDVector::zeros(input_size),
+            biases: Array1::zeros(input_size),
+            activation,
+            m_weights: Array2::zeros((input_size, output_size)),
+            v_weights: Array2::zeros((input_size, output_size)),
+            m_biases: Array1::zeros(input_size),
+            v_biases: Array1::zeros(input_size),
             beta_1: 0.9,
             beta_2: 0.999,
             epsilon: 1e-8,
-            input: DMatrix::zeros(1, 1),
-            output: DMatrix::zeros(1, 1),
+            input: Array2::zeros((1, 1)),
+            output: Array2::zeros((1, 1)),
         }
     }
 
     /// Forward pass
-    ///
-    /// Args:
-    ///
-    /// x: Numerical values of the data (Tensor)
-    pub fn forward(&mut self, x: DMatrix<f64>) -> DMatrix<f64> {
+    pub fn forward(&mut self, x: Array2<f64>) -> Array2<f64> {
         self.input = x.clone();
-        let mut z = &self.input * &self.weights;
-        z.row_iter_mut().for_each(|mut row| row += &self.biases);
+        let mut z = self.input.dot(&self.weights);
+        // Add biases to each row
+        for mut row in z.rows_mut() {
+            row += &self.biases;
+        }
         match self.activation {
             "relu" => {
-                self.output = z.map(|v| if v > 0.0 { v } else { 0.0 });
+                self.output = z.mapv(|v| v.max(0.0));
             }
             "softmax" => {
                 unimplemented!("softmax not yet implemented.")
@@ -85,21 +74,12 @@ impl FullyConnected<'_> {
     }
 
     /// Backpropagation
-    ///
-    /// Args:
-    ///
-    /// d_values: Derivative of the output
-    ///
-    /// learning_rate: Learning rate for gradient descent
-    ///
-    /// t: Timestep
-    pub fn backward(&mut self, d_values: DMatrix<f64>, learning_rate: f64, t: i32) -> DMatrix<f64> {
+    pub fn backward(&mut self, d_values: Array2<f64>, learning_rate: f64, t: i32) -> Array2<f64> {
         let mut d_values = d_values.clone();
         // Calculate the derivative of the activation function
         match self.activation {
             "relu" => {
-                d_values
-                    .component_mul_assign(&self.output.map(|x| if x > 0.0 { 1.0 } else { 0.0 }));
+                d_values *= &self.output.mapv(|x| if x > 0.0 { 1.0 } else { 0.0 });
             }
             "softmax" => {
                 unimplemented!("softmax not yet implemented.")
@@ -109,33 +89,34 @@ impl FullyConnected<'_> {
             }
         }
         // Calculate the derivative with respect to weights and biases
-        let d_weights = self.output.transpose() * &d_values;
-        let d_biases = d_values.row_sum();
+        let d_weights = self.output.t().dot(&d_values);
+        let d_biases = d_values.sum_axis(Axis(0));
         // Clip derivatives to avoid extreme values
-        let d_weights_clipped = d_weights.map(|x| x.max(-1.0).min(1.0));
-        let d_biases_clipped = d_biases.map(|x| x.max(-1.0).min(1.0));
+        let d_weights_clipped = d_weights.mapv(|x| x.max(-1.0).min(1.0));
+        let d_biases_clipped = d_biases.mapv(|x| x.max(-1.0).min(1.0));
         // Calculate gradient with respect to the input
-        let d_inputs = d_values * self.weights.transpose();
-        // Update weights using gradient descent
-        self.weights -= learning_rate * &d_weights_clipped;
-        self.biases -= RowDVector::from(d_biases_clipped.clone()) * learning_rate;
+        let d_inputs = d_values.dot(&self.weights.t());
+        // Update weights and biases using gradient descent
+        self.weights -= &(&d_weights_clipped * learning_rate);
+        self.biases -= &(&d_biases_clipped * learning_rate);
         // Adam optimizer for weights
-        self.m_weights = self.beta_1 * &self.m_weights + (1.0 - self.beta_1) * &d_weights_clipped;
-        self.v_weights =
-            self.beta_2 * &self.v_weights + (1.0 - self.beta_2) * d_weights_clipped.map(|x| x * x);
+        self.m_weights =
+            &(&self.m_weights * self.beta_1) + &(&d_weights_clipped * (1.0 - self.beta_1));
+        self.v_weights = &(&self.v_weights * self.beta_2)
+            + &(&d_weights_clipped.mapv(|x| x * x) * (1.0 - self.beta_2));
         let m_hat_weights = &self.m_weights / (1.0 - self.beta_1.powi(t));
         let v_hat_weights = &self.v_weights / (1.0 - self.beta_2.powi(t));
-        self.weights -= learning_rate
-            * m_hat_weights.component_div(&v_hat_weights.map(|x| x.sqrt() + self.epsilon));
-        // Adam optimizer for biases
-        self.m_biases = self.beta_1 * &self.m_biases + (1.0 - self.beta_1) * &d_biases_clipped;
-        self.v_biases =
-            self.beta_2 * &self.v_biases + (1.0 - self.beta_2) * d_biases_clipped.map(|x| x * x);
+        self.weights -=
+            &(learning_rate * &m_hat_weights / &v_hat_weights.mapv(|x| x.sqrt() + self.epsilon));
+        // Adam oxtimizer for biases
+        self.m_biases =
+            &(&self.m_biases * self.beta_1) + &(&d_biases_clipped * (1.0 - self.beta_1));
+        self.v_biases = &(&self.v_biases * self.beta_2)
+            + &(&d_biases_clipped.mapv(|x| x * x) * (1.0 - self.beta_2));
         let m_hat_biases = &self.m_biases / (1.0 - self.beta_1.powi(t));
         let v_hat_biases = &self.v_biases / (1.0 - self.beta_2.powi(t));
-        self.biases -= RowDVector::from(
-            m_hat_biases.component_div(&v_hat_biases.map(|x| x.sqrt() + self.epsilon)),
-        ) * learning_rate;
+        self.biases -=
+            &(learning_rate * &m_hat_biases / &v_hat_biases.mapv(|x| x.sqrt() + self.epsilon));
         d_inputs
     }
 }
