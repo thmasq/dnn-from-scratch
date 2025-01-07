325a108d2a0832d2271ed05b67f66833b2f1e1ed
Arthur Carvalho <arthur_07q@hotmail.com>
2025-01-07 19:59:14 -0300

    Refactor the argmax function

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -54,24 +54,28 @@ impl NeuralNetwork<'_> {
         total_sum / total_elements
     }
 
-    fn argmax(&self, array: &Array2<f64>, axis: usize) -> Array1<usize> {
-        let axis = Axis(axis);
-        // Validate the axis value
-        assert!(
-            axis.index() < array.ndim(),
-            "Invalid axis: {} for array with {} dimensions",
-            axis.index(),
-            array.ndim()
-        );
-        // Compute the argmax along the specified axis
-        array.map_axis(axis, |subview| {
-            subview
-                .iter()
-                .enumerate()
-                .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                .map(|(idx, _)| idx)
-                .unwrap_or(0) // Default to 0 if subview is empty
-        })
+    fn argmax(&self, arr: &Array2<f64>, axis: usize) -> Array1<usize> {
+        let shape = arr.shape();
+        let (nrows, ncols) = (shape[0], shape[1]);
+        let (upper_bound_i, upper_bound_j) = match axis {
+            0 => (ncols, nrows),
+            1 => (nrows, ncols),
+            _ => panic!("Axis must be 0 or 1"),
+        };
+        let mut result = Vec::with_capacity(upper_bound_i);
+        for i in 0..upper_bound_i {
+            let mut max_val = f64::NEG_INFINITY;
+            let mut max_idx = 0;
+            for j in 0..upper_bound_j {
+                let val = arr[[i, j]];
+                if val > max_val {
+                    max_val = val;
+                    max_idx = j;
+                }
+            }
+            result.push(max_idx);
+        }
+        Array1::from_vec(result)
     }
 
     fn compute_accuracy<T>(&self, output: &Array1<T>, target: &Array1<T>) -> f64
