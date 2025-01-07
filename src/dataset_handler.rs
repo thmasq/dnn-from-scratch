8e8be41741c8a09ea6cdb36b447aa789724106fd
Arthur Carvalho <arthur_07q@hotmail.com>
2025-01-07 14:26:51 -0300

    Refactoring and new fetures on the dataset handler - Replace nalgebra with ndarray so that multi-dimensional arrays are allowed - New function to load the MNIST dataset

--- src/dataset_handler.rs
+++ src/dataset_handler.rs
@@ -1,10 +1,13 @@
-use na::DMatrix;
+use nd::{s, Array2, ArrayD};
+use npy::ReadNpyExt;
 use polars::prelude::*;
+use std::fs::File;
+use std::path::{Path, PathBuf};
 
 pub fn load_rssi_dataset(
     path_to_csv: &str,
     test_proportion: f64,
-) -> (DMatrix<f64>, DMatrix<f64>, DMatrix<f64>, DMatrix<f64>) {
+) -> (ArrayD<f64>, ArrayD<f64>, ArrayD<f64>, ArrayD<f64>) {
     assert!(
         0. < test_proportion && test_proportion < 1.,
         "The test proportion should be in the (0, 1) range."
@@ -18,21 +21,50 @@ pub fn load_rssi_dataset(
         .to_ndarray::<Float64Type>(IndexOrder::C)
         .unwrap();
     let df_nrows = df.nrows();
-    let mut y_matrix = DMatrix::zeros(df_nrows, 2);
-    let mut x_matrix = DMatrix::zeros(df_nrows, 13);
+    let mut y_matrix = Array2::zeros((df_nrows, 2));
+    let mut x_matrix = Array2::zeros((df_nrows, 13));
     for i in 0..df_nrows {
         for j in 1..=2 {
-            y_matrix[(i, j - 1)] = df[[i, j]];
+            y_matrix[[i, j - 1]] = df[[i, j]];
         }
         for j in 3..=15 {
-            x_matrix[(i, j - 3)] = df[[i, j]];
+            x_matrix[[i, j - 3]] = df[[i, j]];
         }
     }
     let num_test = (df_nrows as f64 * test_proportion).round() as usize;
     let num_train = df_nrows - num_test;
-    let x_train = DMatrix::from(x_matrix.rows(0, num_train));
-    let y_train = DMatrix::from(y_matrix.rows(0, num_train));
-    let x_validation = DMatrix::from(x_matrix.rows(num_train, num_test));
-    let y_validation = DMatrix::from(y_matrix.rows(num_train, num_test));
-    (x_train, y_train, x_validation, y_validation)
+    let train_slice = s![num_train..df_nrows, ..];
+    let test_slice = s![num_train..df_nrows, ..];
+    let x_train = x_matrix.slice(train_slice).into_owned().into_dyn();
+    let y_train = y_matrix.slice(train_slice).into_owned().into_dyn();
+    let x_test = x_matrix.slice(test_slice).into_owned().into_dyn();
+    let y_test = y_matrix.slice(test_slice).into_owned().into_dyn();
+    (x_train, y_train, x_test, y_test)
+}
+
+fn read_mnist_npy(path_to_npy: PathBuf) -> ArrayD<u8> {
+    let reader = File::open(path_to_npy).expect("Failure when reading npy file.");
+    let array = ArrayD::<u8>::read_npy(reader).expect("Failure when parsing npy file.");
+    array
+}
+
+pub fn load_mnist_dataset(
+    path_to_folder: &str,
+) -> (ArrayD<u8>, ArrayD<u8>, ArrayD<u8>, ArrayD<u8>) {
+    let files = ["x_train.npy", "y_train.npy", "x_test.npy", "y_test.npy"];
+    let path_to_folder = Path::new(path_to_folder);
+    let mut arrays = [None, None, None, None];
+    for (i, &file) in files.iter().enumerate() {
+        let file = Path::new(file);
+        let filepath = path_to_folder.join(file);
+        let array = read_mnist_npy(filepath);
+        arrays[i] = Some(array);
+    }
+    let (x_train, y_train, x_test, y_test) = (
+        arrays[0].take().unwrap(),
+        arrays[1].take().unwrap(),
+        arrays[2].take().unwrap(),
+        arrays[3].take().unwrap(),
+    );
+    (x_train, y_train, x_test, y_test)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate nalgebra as na;
 extern crate ndarray as nd;
+extern crate ndarray_npy as npy;
 extern crate polars;
 extern crate rand;
 extern crate rand_distr;
@@ -8,13 +9,32 @@ mod dataset_handler;
 mod fully_connected;
 mod neural_network;
 
+use std::env;
+
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+
     const INPUT_SIZE: usize = 784;
     const OUTPUT_SIZE: usize = 10;
     const HIDDEN_SIZES: [usize; 2] = [512, 512];
     let (x_train, y_train, x_test, y_test) =
-        dataset_handler::load_rssi_dataset("static/rssi-dataset.csv", 0.10);
-    let neural_network = neural_network::NeuralNetwork::new(INPUT_SIZE, OUTPUT_SIZE, HIDDEN_SIZES);
-    neural_network.train(x_train, y_train, x_test, y_test, 100, 0.001, 0.001);
-    neural_network.plot();
+        dataset_handler::load_rssi_dataset("assets/rssi-dataset.csv", 0.10);
+    println!(
+        "RSSI dataset shapes: {:?} {:?} {:?} {:?}",
+        x_train.shape(),
+        y_train.shape(),
+        x_test.shape(),
+        y_test.shape()
+    );
+    let (x_train, y_train, x_test, y_test) = dataset_handler::load_mnist_dataset("assets/mnist");
+    println!(
+        "MNIST dataset shapes: {:?} {:?} {:?} {:?}",
+        x_train.shape(),
+        y_train.shape(),
+        x_test.shape(),
+        y_test.shape()
+    );
+    // let neural_network = neural_network::NeuralNetwork::new(INPUT_SIZE, OUTPUT_SIZE, HIDDEN_SIZES);
+    // neural_network.train(x_train, y_train, x_test, y_test, 100, 0.001, 0.001);
+    // neural_network.plot();
 }
